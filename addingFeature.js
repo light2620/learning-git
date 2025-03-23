4becaac2ac900c6964287d75ad19e8c64c3b1793
shivam <shivamnegi896@gmail.com>
2025-03-23 14:03:47 +0530

    deleted all files

--- addingFeature.js
+++
@@ -1,9 +0,0 @@
-console.log("adding feature");
-console.log("added another feature")
-console.log("added third feature");
-console.log("adding some feature");
-
-
-console.log("added 5th feature....")
-console.log("added 6th feature....")
-console.log("added 7th feature...")
--- bugFix.js
+++
@@ -1,2 +0,0 @@
-console.log("bug fixing")
-console.log("fixed another bug");
